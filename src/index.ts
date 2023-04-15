e97b424904cce65aa2d4f97d640ca25c05ac5f2e
Sadman Sakib <ssakib4040@gmail.com>
2023-04-16 00:53:14 +0600

    procedural to oop

--- src/index.ts
+++ src/index.ts
@@ -3,97 +3,95 @@ import Listr from "listr";
 import { promises as fs } from "fs";
 import fse from "fs-extra";
 
-// Backup function
-async function backup(dbUri: string, backupPath: string) {
-  // create a folder named .backup if it doesn't exist
-  await fse.ensureDir(backupPath);
+class BackupToolkit {
+  static async backup(dbUri: string, backupPath: string) {
+    await fse.ensureDir(backupPath);
 
-  const tasks = new Listr([
-    {
-      title: "Connecting to database",
-      task: async () => await mongoose.connect(dbUri),
-    },
-    {
-      title: "Backing up collections",
-      task: async (ctx, task) => {
-        const collections = await mongoose.connection.db.collections();
-        const collectionTasks = collections.map((collection) => ({
-          title: `Backing up ${collection.collectionName}`,
-          task: async () => {
-            const docs = await collection.find().toArray();
-            const filename = `${backupPath}/${collection.collectionName}.json`;
-            await fs.writeFile(filename, JSON.stringify(docs));
-          },
-        }));
-        return new Listr(collectionTasks, { concurrent: true });
+    const tasks = new Listr([
+      {
+        title: "Connecting to database",
+        task: async () => await mongoose.connect(dbUri),
       },
-    },
-    {
-      title: "Backup completed",
-      task: () => console.log("Backup completed ✔"),
-    },
-  ]);
+      {
+        title: "Backing up collections",
+        task: async (ctx, task) => {
+          const collections = await mongoose.connection.db.collections();
+          const collectionTasks = collections.map((collection) => ({
+            title: `Backing up ${collection.collectionName}`,
+            task: async () => {
+              const docs = await collection.find().toArray();
+              const filename = `${backupPath}/${collection.collectionName}.json`;
+              await fs.writeFile(filename, JSON.stringify(docs));
+            },
+          }));
+          return new Listr(collectionTasks, { concurrent: true });
+        },
+      },
+      {
+        title: "Backup completed",
+        task: () => console.log("Backup completed ✔"),
+      },
+    ]);
 
-  await tasks.run();
-  // console.log("Backup completed ✔");
-  mongoose.connection.close();
-}
-
-// Restore function
-async function restore(dbUri: string, backupPath: string) {
-  // if folder doesn't exist
-  if (!(await isDirectoryExists(backupPath))) {
-    console.log("Error: Backup folder doesn't exist");
-    return;
+    await tasks.run();
+    mongoose.connection.close();
   }
 
-  const tasks = new Listr([
-    {
-      title: "Connecting to database",
-      task: async () => await mongoose.connect(dbUri),
-    },
-    {
-      title: "Restoring collections",
-      task: async (ctx, task) => {
-        const filenames = await fs.readdir(backupPath);
-        const collectionTasks = filenames.map((filename) => ({
-          title: `Restoring ${filename}`,
-          task: async () => {
-            const collectionName = filename.slice(0, -5); // remove ".json" extension
-            const collection =
-              mongoose.connection.db.collection(collectionName);
-            const data = await fs.readFile(`${backupPath}/${filename}`, "utf8");
-            const docs = JSON.parse(data);
-            await collection.deleteMany({});
-            await collection.insertMany(docs);
-          },
-        }));
-        return new Listr(collectionTasks, { concurrent: true });
+  static async restore(dbUri: string, backupPath: fse.PathLike) {
+    if (!(await BackupToolkit.isDirectoryExists(backupPath))) {
+      console.log("Error: Backup folder doesn't exist");
+      return;
+    }
+
+    const tasks = new Listr([
+      {
+        title: "Connecting to database",
+        task: async () => await mongoose.connect(dbUri),
       },
-    },
-    {
-      title: "Restore completed",
-      task: () => console.log("Restore completed"),
-    },
-  ]);
+      {
+        title: "Restoring collections",
+        task: async (ctx, task) => {
+          const filenames = await fs.readdir(backupPath);
+          const collectionTasks = filenames.map((filename) => ({
+            title: `Restoring ${filename}`,
+            task: async () => {
+              const collectionName = filename.slice(0, -5);
+              const collection =
+                mongoose.connection.db.collection(collectionName);
+              const data = await fs.readFile(
+                `${backupPath}/${filename}`,
+                "utf8"
+              );
+              const docs = JSON.parse(data);
+              await collection.deleteMany({});
+              await collection.insertMany(docs);
+            },
+          }));
+          return new Listr(collectionTasks, { concurrent: true });
+        },
+      },
+      {
+        title: "Restore completed",
+        task: () => console.log("Restore completed"),
+      },
+    ]);
 
-  await tasks.run();
-  mongoose.connection.close();
-}
+    await tasks.run();
+    mongoose.connection.close();
+  }
 
-async function isDirectoryExists(path: string): Promise<boolean> {
-  try {
-    const stats = await fs.stat(path);
-    return stats.isDirectory();
-  } catch (error: any) {
-    if (error.code === "ENOENT") {
-      // The directory does not exist
-      return false;
-    } else {
-      // Some other error occurred
-      throw error;
+  static async isDirectoryExists(path: fse.PathLike) {
+    try {
+      const stats = await fs.stat(path);
+      return stats.isDirectory();
+    } catch (error: any) {
+      if (error.code === "ENOENT") {
+        return false;
+      } else {
+        throw error;
+      }
     }
   }
 }
 
-export { backup, restore, isDirectoryExists };
+export default BackupToolkit;
--- tests/core.test.ts
+++ tests/core.test.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { expect } from "chai";
-import { backup, restore, isDirectoryExists } from "../src/index";
+import Core from "../src/index";
 
 describe("mongodb-backup-toolkit", () => {
   const backupPath = "tests/backup";
@@ -8,13 +8,13 @@ describe("mongodb-backup-toolkit", () => {
   before(async () => {
     // Connect to database before running tests
     const dbUri = "mongodb://127.0.0.1:27017/test";
-    await backup(dbUri, backupPath);
+    await Core.backup(dbUri, backupPath);
   });
 
   after(async () => {
     // Restore the database after running tests
     const dbUri = "mongodb://127.0.0.1:27017/test";
-    await restore(dbUri, backupPath);
+    await Core.restore(dbUri, backupPath);
 
     fs.rmdirSync(backupPath, { recursive: true });
   });
@@ -22,9 +22,9 @@ describe("mongodb-backup-toolkit", () => {
   describe("#backup()", () => {
     it("should create a backup of the database", async () => {
       const dbUri = "mongodb://127.0.0.1:27017/test";
-      await backup(dbUri, backupPath);
+      await Core.backup(dbUri, backupPath);
       // Check if backup directory exists
-      const directoryCheck: any = await isDirectoryExists(backupPath);
+      const directoryCheck: any = await Core.isDirectoryExists(backupPath);
       expect(directoryCheck).to.be.true;
     });
   });
@@ -32,7 +32,7 @@ describe("mongodb-backup-toolkit", () => {
   describe("#restore()", () => {
     it("should restore the database from a backup", async () => {
       const dbUri = "mongodb://127.0.0.1:27017/test";
-      await restore(dbUri, backupPath);
+      await Core.restore(dbUri, backupPath);
       // Check if database was restored successfully
     });
   });
